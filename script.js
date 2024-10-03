a5b9e12ce1e783ec1f508922f889381bcefc34f1
Thais Guedes <thais.guedes@ufpe.br>
2024-10-03 19:44:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//A validação do formulário com javascript é considerado um desafio extra, não é preciso fazer esta validação para realizar sua entrega
+// Função para scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        // Verifica se o elemento alvo existe
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
